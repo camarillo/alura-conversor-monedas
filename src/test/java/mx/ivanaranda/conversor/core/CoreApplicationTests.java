d7b1d4ff273a4f8a778ba5cd6759e93132c11fcd
Ivan Aranda <camarillo.ivan@gmail.com>
2024-07-15 19:30:57 -0600

    feat: Agregando estructura inicial del proyecto.

---
+++ src/test/java/mx/ivanaranda/conversor/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.ivanaranda.conversor.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
